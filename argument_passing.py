5aaf71a36a5053600e05674613b0462d84b82dc6
jiangwei <jiangwei1993@gmail.com>
2021-02-17 21:45:24 -0800

    python/np argument

---
+++ argument_passing.py
@@ -0,0 +1,27 @@
+'''
+python/numpy argument passing and assignment
+'''
+import numpy as np
+
+
+def i_will_modify_x(x):
+    x += 1
+
+
+def i_wont_modify_x(x):
+    x = x.copy()
+    x += 1
+
+
+def main():
+    x = np.array([1, 2, 3, 4])
+    print('original x:', x)
+    i_will_modify_x(x)
+    print('inplace modified x: ', x)
+    x = np.array([1, 2, 3, 4])
+    i_wont_modify_x(x)
+    print('x stay the same: ', x)
+
+
+if __name__ == "__main__":
+    main()
