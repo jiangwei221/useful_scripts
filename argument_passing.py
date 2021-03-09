abf199df63bc857984224a17764a7bc483dfb0af
jiangwei <jiangwei1993@gmail.com>
2021-03-09 11:10:21 -0800

    updt

--- argument_passing.py
+++ argument_passing.py
@@ -13,11 +13,19 @@ def i_wont_modify_x(x):
     x += 1
 
 
+def i_will_modify_x2(x):
+    y = x
+    y += 1
+
+
 def main():
     x = np.array([1, 2, 3, 4])
     print('original x:', x)
     i_will_modify_x(x)
     print('inplace modified x: ', x)
+    x = np.array([1, 2, 3, 4])
+    i_will_modify_x2(x)
+    print('assign x to y: ', x)
     x = np.array([1, 2, 3, 4])
     i_wont_modify_x(x)
     print('x stay the same: ', x)
